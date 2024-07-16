988c1d8059d8fa0e99550d4f00b161c7acb14681
Victor Cespon Privat <vprivat@springglobal.com>
2024-07-16 18:39:16 -0300

    New task

--- Trello/src/components/TaskList.tsx
+++ Trello/src/components/TaskList.tsx
@@ -1,4 +1,11 @@
-import { View, Text, FlatList } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  TextInput,
+  Button,
+  StyleSheet,
+} from "react-native";
 import TaskListItem from "./TaskListItem";
 import { useState } from "react";
 
@@ -8,14 +15,49 @@ export default function TaskList() {
     { description: "Second Task" },
   ]);
 
+  const [newTask, setNewTask] = useState("");
+
+  const createTask = () => {
+    console.warn("Create: ", newTask);
+    setTasks([...tasks, { description: newTask }]);
+    setNewTask("");
+  };
+
   return (
-    <View style={{ backgroundColor: "#101112", padding: 10, borderRadius: 5 }}>
-      <Text style={{ color: "white", fontWeight: "bold" }}>To Do</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>To Do</Text>
+
       <FlatList
         data={tasks}
         contentContainerStyle={{ gap: 5 }}
         renderItem={({ item }) => <TaskListItem task={item} />}
       />
+
+      <TextInput
+        value={newTask}
+        onChangeText={setNewTask}
+        placeholder="New task"
+        placeholderTextColor="gray"
+        style={styles.input}
+      ></TextInput>
+
+      <Button title="Add Task" onPress={createTask} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#101112",
+    padding: 15,
+    borderRadius: 5,
+    gap: 5,
+  },
+  title: { color: "white", fontWeight: "bold" },
+  input: {
+    color: "white",
+    padding: 15,
+    backgroundColor: "#1D2125",
+    borderRadius: 5,
+  },
+});
