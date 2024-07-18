ee5dd341567728e8400a064c3cea141bdfc608da
Victor Cespon Privat <vprivat@springglobal.com>
2024-07-18 07:48:46 -0300

    Add realm and configure custom settings deploy

--- Trello/src/app/_layout.tsx
+++ Trello/src/app/_layout.tsx
@@ -2,11 +2,14 @@ import { StyleSheet, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { Stack } from "expo-router";
 import { ThemeProvider, DarkTheme } from "@react-navigation/native";
+import Realm from "../providers/Realm";
 
 export default function RootLayout() {
   return (
     <ThemeProvider value={DarkTheme}>
-      <Stack screenOptions={{}} />
+      <Realm>
+        <Stack screenOptions={{}} />
+      </Realm>
       <StatusBar style="light" />
     </ThemeProvider>
   );
