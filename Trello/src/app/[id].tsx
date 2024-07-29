d77c797341b8de9b14d63dfa57c2d083182d7312
Victor Cespon Privat <vprivat@springglobal.com>
2024-07-29 20:33:12 -0300

    úpdating data

--- Trello/src/app/[id].tsx
+++ Trello/src/app/[id].tsx
@@ -1,13 +1,27 @@
 import { Stack, useLocalSearchParams } from "expo-router";
-import { View, Text } from "react-native";
-import { useObject } from "@realm/react";
+import { View, Text, TextInput, StyleSheet } from "react-native";
+import { useObject, useRealm } from "@realm/react";
 import { Task } from "../models/Task";
 import { BSON, Realm } from "realm";
+import { useState } from "react";
 
 export default function TaskDetails() {
   const { id } = useLocalSearchParams();
-
   const task = useObject<Task>(Task, new BSON.ObjectID(id as string));
+
+  const [updatedDescription, setUpdatedDescription] = useState(
+    task?.description
+  );
+
+  const realm = useRealm();
+
+  const updateDescription = () => {
+    if (!task) return;
+
+    realm.write(() => {
+      task.description = updatedDescription;
+    });
+  };
 
   if (!task) {
     return <Text>Not found</Text>;
@@ -16,19 +30,32 @@ export default function TaskDetails() {
   return (
     <View style={{ padding: 10 }}>
       <Stack.Screen options={{ title: "Task Details" }} />
-      <Text style={{ color: "white", fontSize: 20 }}>
-        id: {task._id.toString()}
-      </Text>
-      <Text style={{ color: "white", fontSize: 20 }}>
-        description: {task.description}
-      </Text>
-      <Text style={{ color: "white", fontSize: 20 }}>User: {task.user_id}</Text>
-      <Text style={{ color: "white", fontSize: 20 }}>
-        Complete: {task.isComplete ? "Yes" : "No"}
-      </Text>
-      <Text style={{ color: "white", fontSize: 20 }}>
+      <Text style={styles.text}>id: {task._id.toString()}</Text>
+      <Text style={styles.text}>
         Create Date: {task.createdAt.toDateString()}
       </Text>
+      <Text style={styles.text}>
+        Complete: {task.isComplete ? "Yes" : "No"}
+      </Text>
+      <Text style={styles.text}>User: {task.user_id}</Text>
+      <TextInput
+        value={updatedDescription}
+        onChangeText={setUpdatedDescription}
+        onEndEditing={updateDescription}
+        style={styles.input}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  text: {
+    color: "white",
+    fontSize: 20,
+  },
+  input: {
+    color: "white",
+    fontSize: 20,
+    backgroundColor: "green",
+  },
+});
