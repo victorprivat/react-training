84764bda43f6c55be652f96f36c95e2ef84b4652
Victor Cespon Privat <vprivat@springglobal.com>
2024-07-29 20:07:31 -0300

    Details screen adjust

--- Trello/src/app/[id].tsx
+++ Trello/src/app/[id].tsx
@@ -1,12 +1,34 @@
 import { Stack, useLocalSearchParams } from "expo-router";
 import { View, Text } from "react-native";
+import { useObject } from "@realm/react";
+import { Task } from "../models/Task";
+import { BSON, Realm } from "realm";
 
 export default function TaskDetails() {
   const { id } = useLocalSearchParams();
+
+  const task = useObject<Task>(Task, new BSON.ObjectID(id as string));
+
+  if (!task) {
+    return <Text>Not found</Text>;
+  }
+
   return (
     <View style={{ padding: 10 }}>
       <Stack.Screen options={{ title: "Task Details" }} />
-      <Text style={{ color: "white", fontSize: 20 }}>Id: {id}</Text>
+      <Text style={{ color: "white", fontSize: 20 }}>
+        id: {task._id.toString()}
+      </Text>
+      <Text style={{ color: "white", fontSize: 20 }}>
+        description: {task.description}
+      </Text>
+      <Text style={{ color: "white", fontSize: 20 }}>User: {task.user_id}</Text>
+      <Text style={{ color: "white", fontSize: 20 }}>
+        Complete: {task.isComplete ? "Yes" : "No"}
+      </Text>
+      <Text style={{ color: "white", fontSize: 20 }}>
+        Create Date: {task.createdAt.toDateString()}
+      </Text>
     </View>
   );
 }
