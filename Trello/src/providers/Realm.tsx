5260fda0f0fd55217f04fad9a64efd403568882c
Victor Cespon Privat <vprivat@springglobal.com>
2024-07-29 21:52:07 -0300

    setting up App Provider and Anonymous Login

--- Trello/src/providers/Realm.tsx
+++ Trello/src/providers/Realm.tsx
@@ -1,8 +1,16 @@
 import { PropsWithChildren } from "react";
-import Realm from "realm";
-import { RealmProvider } from "@realm/react";
+import { RealmProvider, AppProvider, UserProvider } from "@realm/react";
 import { Task } from "../models/Task";
+import Login from "../components/Login";
+
+const appId = "application-0-ukvxspk";
 
 export default function RealmCustomProvider({ children }: PropsWithChildren) {
-  return <RealmProvider schema={[Task]}>{children}</RealmProvider>;
+  return (
+    <AppProvider id={appId}>
+      <UserProvider fallback={Login}>
+        <RealmProvider schema={[Task]}>{children}</RealmProvider>
+      </UserProvider>
+    </AppProvider>
+  );
 }
